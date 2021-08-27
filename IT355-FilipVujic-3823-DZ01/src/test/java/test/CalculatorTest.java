720a3fc192a1f6daa08a90d0e6151989573f87ad
Filip-Lenovo <filip.vujic665@gmail.com>
2021-08-27 12:45:10 +0200

    Finished DZ01.

--- IT355-FilipVujic-3823-DZ01/src/test/java/test/CalculatorTest.java
+++ IT355-FilipVujic-3823-DZ01/src/test/java/test/CalculatorTest.java
@@ -5,8 +5,9 @@
  */
 package test;
 
+import dz01.Calculator;
 import org.junit.Test;
-
+import org.junit.Assert;
 /**
  *
  * @author filip
@@ -14,7 +15,26 @@ import org.junit.Test;
 public class CalculatorTest {
     
     @Test
-    public int testMultiply() {
-        
+    public void testMultiply() {
+        Calculator c = new Calculator();
+        Assert.assertEquals(20, c.multiply(5, 4));
+    }
+    
+    @Test
+    public void testAdd() {
+        Calculator c = new Calculator();
+        Assert.assertEquals(10, c.add(4, 6));
+    }
+    
+    @Test
+    public void testSubtract() {
+        Calculator c = new Calculator();
+        Assert.assertEquals(6, c.subtract(7, 1));
+    }
+    
+    @Test
+    public void testDivide() {
+        Calculator c = new Calculator();
+        Assert.assertEquals(2, c.divide(8, 4));
     }
 }
